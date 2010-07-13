0b053fe58ab75d3055ece1699b5a0ac439a2ef48
origami.japes <origami.japes@d18225d7-08b7-fcd1-0b8c-33be1e7320b0>
2010-07-13 06:03:26 +0000

    issues with Drag&Drop.  need wxPython to run

---
+++ stub.py
@@ -0,0 +1,30 @@
+from GUImodule import GUIThread
+import random
+import threading
+import time
+import wx
+
+dart = (0,0,0)
+
+def updateUIfunction():
+    for i in range(12):
+        global dart
+        time.sleep(1)
+        dart = (0, random.random(),random.random())
+        #print dart
+        updateUI.set()
+
+class AppGUI(wx.App):
+    def OnInit(self):
+        self.frame = GUIThread() 
+        return True
+
+if __name__ == '__main__':
+    updateUI = threading.Event()
+    correctScore = threading.Event()
+    
+    updateUIstub = threading.Thread(target=updateUIfunction)
+    updateUIstub.start()
+    
+    app = AppGUI(0)
+    app.MainLoop()
