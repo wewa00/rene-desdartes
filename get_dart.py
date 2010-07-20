324bf5de7ed96b9e77793c138776bcfa61fc7ced
mostlyharmlessben <mostlyharmlessben@d18225d7-08b7-fcd1-0b8c-33be1e7320b0>
2010-07-20 21:48:34 +0000

    calibration can now be done with still image or camera - adjust the global variable from_camera to switch
    get_dart can now also get the dart location from a still image or a video feed. Right now only debug mode is supported (where you indicate a dart throw with a mouse click). Next I will add the actual functionality to get dart throws from just the video

--- get_dart.py
+++ get_dart.py
@@ -1,91 +1,67 @@
 #! /usr/bin/env python
-#import sys
-#import urllib2
 import cv
 from threading import Thread
 from threading import Event
-#import time
-#import math
+
 import dartRegion
 import calibration
 import GameEngine
 
 # some definitions
 window_name = "Get Dart Location"
+debug = True
+from_camera = True
 
 def on_mouse(event, x, y, flags, param):
     if event==cv.CV_EVENT_LBUTTONDOWN:
-##        print "x-pos: %d" % x
-##        print "y-pos: %d" % y
-
-        #events
-        #global points
         global mouse_click_down
         global x_coordinate
         global y_coordinate
         x_coordinate = x
         y_coordinate = y
-        #counter
-        #append user clicked points
-        #points.append((x,y))
         mouse_click_down.set()
 
-
-def GetDartWindowThread(im):
-    cv.ShowImage(window_name, im)
-
-    cv.SetMouseCallback(window_name, on_mouse)
-    global done_clicking
-
-    while True:
-        if not done_clicking.is_set():
-            cv.WaitKey(1)
-        else:
-            break
-
-    global x_coordinate
-    global y_coordinate
-    cv.Circle(im,(x_coordinate,y_coordinate),3,cv.CV_RGB(255, 0, 0),2)
-    cv.ShowImage(window_name, im)
-    cv.WaitKey(0)
-
 def GetDart():
-        #capture = cv.CaptureFromCAM(0)
-    #image = 0
+    if debug:
+        if from_camera:
+            capture = cv.CaptureFromCAM(0)
+            cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+            cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+            image = 0
 
-    #cv.GrabFrame(capture)
-    #image = cv.RetrieveFrame(capture)
-    #cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 1280)
-    #cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 720)
-    #image = cv.LoadImage(str(r"Dartboard Left.jpg"),cv.CV_LOAD_IMAGE_COLOR)
-    image = calibration.image
+            cv.GrabFrame(capture)
+            image = cv.RetrieveFrame(capture)           
+        else:            
+            #image = cv.LoadImage(str(r"Dartboard Left.jpg"),cv.CV_LOAD_IMAGE_COLOR)
+            image = calibration.image   
 
-    # the coordinates
-    global x_coordinate
-    global y_coordinate
-    x_coordinate = 0
-    y_coordinate = 0
+        # the coordinates
+        global x_coordinate
+        global y_coordinate
+        x_coordinate = 0
+        y_coordinate = 0
 
-    #use two events to emulate wait for mouse click event
-    global mouse_click_down
-    global done_clicking
-    mouse_click_down = Event()
-    done_clicking = Event()
-    t = Thread(target=GetDartWindowThread,args=(image,));
-    t.start()
+        #use this events wait for mouse click
+        global mouse_click_down
+        mouse_click_down = Event()
 
-    mouse_click_down.wait()
-    mouse_click_down.clear()
-    #cv.Circle(image,(x_coordinate,y_coordinate),1,cv.CV_RGB(255, 0, 0),5)
-    #cv.ShowImage(window_name,image)
+        cv.NamedWindow(window_name, 1)
+        cv.SetMouseCallback(window_name, on_mouse)
+        
+        while not mouse_click_down.is_set():
+            if from_camera:
+                cv.GrabFrame(capture)
+                image = cv.RetrieveFrame(capture)
+            cv.ShowImage(window_name, image)
+            cv.WaitKey(1)
+        mouse_click_down.clear()
 
+        cv.Circle(image,(x_coordinate,y_coordinate),3,cv.CV_RGB(255, 0, 0),2)
+        cv.ShowImage(window_name, image)
 
-    #destroy GetDart window
-    done_clicking.set()
-
-    #wait a key pressed to end
-    #cv.WaitKey(0)
-    return (x_coordinate,y_coordinate)
+        return (x_coordinate,y_coordinate)
+    else: 
+        pass
 
 if __name__ == '__main__':
     #calibrate first
@@ -103,6 +79,7 @@ if __name__ == '__main__':
     print correct_dart_location
 
     new_image = cv.CloneImage(calibration.image)
+    
     cv.WarpPerspective(calibration.image, new_image, calibration.mapping)
     cv.Circle(new_image, correct_dart_location, 3,cv.CV_RGB(255, 0, 0),2, 8)
     cv.ShowImage("new dart location",new_image)
